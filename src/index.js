67bb2475d33215602ef979b911e134d6d0617d43
vedansh36 <146646171+vedansh36@users.noreply.github.com>
2025-05-15 18:59:06 +0530

    util documnets added

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+const app = express();
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed !! ", err);
+  });
+/* FIRST APPROACH
+(async () => 
+  try {
+    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error",(error)=>{
+        console.log("ERROR",error);
+        throw error
+    })
+
+    app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
