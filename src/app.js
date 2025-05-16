d72d9d09bb004962ed51116b52d7b92a43bd485b
vedansh36 <146646171+vedansh36@users.noreply.github.com>
2025-05-16 20:59:12 +0530

    first route added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import cors from 'cors'
-const app=express()
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+const app = express();
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 
-export {app}
+//routes import
+import userRouter from "./routes/user.routes.js";
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
-import express from "express";
-const app = express();
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config();
