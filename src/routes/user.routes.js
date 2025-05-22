435c1df31703884cc185cb759ea963801ea4f75b
vedansh36 <146646171+vedansh36@users.noreply.github.com>
2025-05-22 23:16:46 +0530

    added refreshAccessToken and accessToken

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,29 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser, logOutUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logOutUser,
+  refreshAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 router.route("/register").post(
-    upload.fields([
-        {
-            name:"avatar",
-            maxCount:1,  
-        },
-        {
-            name:"coverImage",
-            maxCount:1,
-        }
-    ]),
-    registerUser);
-router.route("/login").post(loginUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logOutUser)
+router.route("/logout").post(verifyJWT, logOutUser);
+router.route("refresh-token").post(refreshAccessToken);
 export default router;
